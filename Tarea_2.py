b671e7aff3954b7c8de73628c2d83ae8ff98819a
jeffGS05 <49353053+jeffGS05@users.noreply.github.com>
2019-04-10 13:38:06 -0600

    Update Tarea_2.py

--- Tarea_2.py
+++ Tarea_2.py
@@ -1,13 +1,3 @@
-######### Crear líneas de código en Python que calcule el promedio de los valores contenidos en una lista.#####
-
-New_list = [27,45,87,23,45,65]
-def Average(New_list):
-    return sum(New_list) / len(New_list)
-
-average = Average(New_list)
-# Printing average of the list
-print("Average of the New_ list is =", round(average, 2))
-
 ### Escriba un código en python que determine cual grupo de personas contiene la mayor de todas las alturas de todas las personas ###
 
 group1 =[177,145,167,190,140,150,180,130]
@@ -18,30 +8,21 @@ group3 =[145,136,178,200,123,145,145,134]
 
 group4 =[201,110,187,175,156,165,156,135]
 
-def Average1 (group1):
-    return sum(group1) / len(group1)
-average1 = Average1(group1)
-print(round(average1,2))
+def Average(group):
+    average = sum(group) / len(group)
+    return average
 
-def Average2 (group2):
-    return sum(group2) / len(group2)
-average2 = Average2(group2)
-print(round(average2,2))
+average1 = Average(group1)
+print("Group1, " + str(round(average1, 2)))
 
-def Average3 (group3):
-    return sum(group3) / len(group3)
-average3 = Average3(group3)
-print(round(average3,2))
+average2 = Average(group2)
+print("Group2, " + str(round(average2, 2)))
 
-def Average4 (group4):
-    return sum(group4) / len(group4)
-average4 = Average4(group4)
+average3 = Average(group3)
+print("Group3, " + str(round(average3, 2)))
 
-print(round(average4,2))
+average4 = Average(group4)
+print("Group4, " + str(round(average4, 2)))
 
 highestscore = max((average1,average2,average3,average4))
-print("The group with the people most tall is the group4 with",  highestscore)
-
-
-
-
+print("The group with the people most tall is the group2 with",  highestscore)
